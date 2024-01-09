6941c36b2953bfe8fc04f732a2c84dffe47b46aa
Lawson Lewis <lawson@kurrawong.ai>
2024-01-09 16:35:20 +1000

    remove dataset. add creationdate, serialize to longturtle

--- etl.py
+++ etl.py
@@ -2,24 +2,24 @@
 Convert ORIC extract from csv to RDF
 """
 import pandas as pd
-from rdflib import Graph, URIRef, Namespace, Literal, Dataset, BNode
-from rdflib.namespace import RDF, RDFS, SDO, NamespaceManager
+from rdflib import Graph, URIRef, Namespace, Literal, Dataset
+from rdflib.namespace import RDF, SDO
+from datetime import date
 
 
 def main():
-    # set up the Graph
-    g = Graph(identifier="https://example.org/oric")
-    d = Dataset()
-    dataset_iri = URIRef("https://example.org/oric")
+    g = Graph()
+    dataset_iri = URIRef("https://example.org/oric-extract")
     DS = Namespace(str(dataset_iri) + "/")
     IDNCP = Namespace("https://data.idnau.org/pid/cp/")
-    nm = NamespaceManager(Graph())
-    nm.bind("idncp", IDNCP, override=False)
-    g.namespace_manager = nm
+    g.bind("idncp", IDNCP)
+
+    g.add((dataset_iri, SDO.dateCreated, Literal(date.today(), datatype=SDO.Date)))
 
     df = pd.read_csv("oric_corps2023.csv", encoding="ISO-8859-1")
     for index, row in df.iterrows():
         item_iri = URIRef(DS + str(row["ICN"]))
+        g.add((dataset_iri, SDO.hasPart, item_iri))
         g.add((item_iri, RDF.type, SDO.Organization))
         if row["Status"] == "Registered":
             g.add(
@@ -51,9 +51,10 @@ def main():
                 Literal(str(row["Numbe of Members"]), datatype=SDO.Number),
             )
         )
-        g.add((item_iri, RDFS.seeAlso, Literal(str(row["URL"]), datatype=SDO.URL)))
-    d.add_graph(g)
-    d.serialize(destination='oric.nq', format="nquads")
+        g.add((item_iri, SDO.url, Literal(str(row["URL"]), datatype=SDO.URL)))
+        if index > 1:
+            break
+    g.serialize(destination='oric.ttl', format="longturtle")
 
 
 if __name__ == "__main__":
